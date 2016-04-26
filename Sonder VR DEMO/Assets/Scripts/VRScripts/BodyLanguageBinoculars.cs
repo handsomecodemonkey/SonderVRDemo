d4c3a67c343c32dd3a668911c855219998e5c299
Erick Kusnadi <erick@handsomecodemonkey.com>
2016-04-26 16:54:00 -0700

    Start work on Body language binoculars intro

---
+++ Sonder VR DEMO/Assets/Scripts/VRScripts/BodyLanguageBinoculars.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class BodyLanguageBinoculars : VRInteractiveItem
+{
+	// Use this for initialization
+	void Start () {
+		this.OnOver += HandleOver;
+		this.OnOut += HandleOut;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//Handle the Over event
+	private void HandleOver()
+	{
+		//transform.localScale = originalScale * 2;
+		Debug.Log("Show over state");
+
+	}
+
+	//Handle the Out Event
+	private void HandleOut()
+	{
+		//transform.localScale = originalScale;
+		Debug.Log("Show out state");
+	}
+}
